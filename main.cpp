f03656bea7d9a19308ece6b11ab2ed76d8739042
Robinson Correa Morales <robinson.corream@udea.edu.co>
2023-09-14 22:15:19 -0500

    "primeras configuraciones de arduino"

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
-#include <iostream>
 
-using namespace std;
+#define latch 3 //Cable verde
+#define clock 2 //Cable azul
+#define datos 5 //Cable amarillo
 
-int main()
-{
-    //text
-    cout << "Hello World!" << endl;
-    return 0;
+
+void setup() {
+    pinMode(latch,OUTPUT);
+    pinMode(clock,OUTPUT);
+    pinMode(datos,OUTPUT);
+
+    randomSeed(analogRead(0));
 }
+
+void loop() {
+
+}
+
