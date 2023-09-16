e0f8e5d4f80f42a46899cdfeb45ad95422fdb6f3
Robinson Correa Morales <robinson.corream@udea.edu.co>
2023-09-15 19:07:55 -0500

    "Creación de funcion impresion"

--- main.cpp
+++ main.cpp
@@ -1,18 +1,24 @@
-
 #define latch 3 //Cable verde
 #define clock 2 //Cable azul
 #define datos 5 //Cable amarillo
 
+void dibujar(char *fila, char *columna) {
+    shiftOut(datos,clock,MSBFIRST,fila);  //catodos largo - columnas, estos funcionan inverso, para que el led se encienda debe ser 0 y no 1
+    shiftOut(datos,clock,MSBFIRST,columna); //Anodos altura - filas
+    digitalWrite(latch,HIGH);
+    digitalWrite(latch,LOW);
+}
 
 void setup() {
+
+    Serial.begin(9600);
     pinMode(latch,OUTPUT);
     pinMode(clock,OUTPUT);
     pinMode(datos,OUTPUT);
-
-    randomSeed(analogRead(0));
 }
 
 void loop() {
 
-}
 
+
+}
